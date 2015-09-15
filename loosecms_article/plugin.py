09dd3a6f57453e38a1ce277b066f49ddb9145371
Lefteris Nikoltsios <lefteris.nikoltsios@gmail.com>
2015-09-15 13:11:44 +0300

    Fix fetching articles from current manager (Closes #1).

--- loosecms_article/plugin.py
+++ loosecms_article/plugin.py
@@ -36,20 +36,25 @@ class ArticleManagerPlugin(PluginModelAdmin):
                 context['slug'] = context['kwargs']['slug']
                 '''Fetch specific article'''
                 try:
-                    articles = Article.objects.select_related().get(published=True, slug=context['slug'])
+                    articles = Article.objects.select_related()\
+                        .get(published=True, slug=context['slug'], manager=manager)
                 except Article.DoesNotExist:
                     raise Http404
             elif 'category_slug' in context['kwargs']:
                 context['category_slug'] = context['kwargs']['category_slug']
 
                 '''Fetch all articles for requested category'''
-                articles = Article.objects.select_related().filter(published=True, category__slug=context['category_slug']).order_by('-ctime')
+                articles = Article.objects.select_related()\
+                    .filter(published=True, category__slug=context['category_slug'], manager=manager)\
+                    .order_by('-ctime')
                 if len(articles) == 0:
                     raise Http404
 
         elif context['page_slug'] != '':
             ''' Fetch all articles for requested page'''
-            articles = Article.objects.select_related().filter(published=True).order_by('-ctime')
+            articles = Article.objects.select_related()\
+                .filter(published=True, manager=manager)\
+                .order_by('-ctime')
 
         if isinstance(articles, QuerySet):
             paginator = Paginator(articles, manager.number)
